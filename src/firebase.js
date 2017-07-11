6ae802883da67e455d00698f31b0eb53111da92e
[01;31m[K"name"[m[K: "Gina Benavidez <[01;31m[K"email"[m[K: null>
2017-07-10 20:19:44 -0700

    create a firebase file and import firebase, then initialize with the firebase console's api key

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyCZ7Z8BRk86TwkAokjvektiGyXM2zZDdLw",
+    authDomain: "clearly-now-app.firebaseapp.com",
+    databaseURL: "https://clearly-now-app.firebaseio.com",
+    projectId: "clearly-now-app",
+    storageBucket: "clearly-now-app.appspot.com",
+    messagingSenderId: "894780155237"
+  };
+
+firebase.initializeApp(config);
+
